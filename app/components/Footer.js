f7cbe4534c693386fcba1170c628cdfe247c65c9
Joshua Zapata <joshuazap@gmail.com>
2016-04-28 15:30:26 -0700

    Minor tweak to footer heading

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -39,7 +39,7 @@ class Footer extends React.Component {
         <div className='container'>
           <div className='row'>
             <div className='col-sm-5'>
-              <h3 className='lead'><strong>Information</strong> and <strong>Copyright</strong></h3>
+              <h3 className='lead'><strong>Information</strong></h3>
               <p>Powered by <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>React</strong> with Flux architecture and server-side rendering.</p>
             </div>
             <div className='col-sm-7 hidden-xs'>
